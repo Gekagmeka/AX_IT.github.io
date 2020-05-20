5ccb249ea078c68548e14057253bd1a427cba93d
YS <Shulgagggeka@gmail.com>
2020-05-20 23:41:41 +0300

    begin slick

--- js/js.js
+++ js/js.js
@@ -2,3 +2,11 @@ $('.header__burger, .header__link').on('click', function(){
 	$('.header__serif, .header__burger, .header__navigation').toggleClass('active');
 	$('body').toggleClass('locked');
 });
+
+// carousel
+$('.slick').slick({
+	dots: true,
+	spped: 300,
+	slidesToShow: 1,
+	arrows: false
+});
